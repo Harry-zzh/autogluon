d7403c3587af29abbf99d29e638892c6f0e243c0
Ubuntu <ubuntu@ip-172-31-9-160.us-west-2.compute.internal>
2024-04-16 14:36:18 +0000

    clip miss token and fix clip zero image bug

--- multimodal/src/autogluon/multimodal/utils/model.py
+++ multimodal/src/autogluon/multimodal/utils/model.py
@@ -209,6 +209,7 @@ def create_model(
             num_classes=num_classes,
             pretrained=pretrained,
             tokenizer_name=model_config.tokenizer_name,
+            use_miss_token_embed=model_config.use_miss_token_embed
         )
     elif model_name.lower().startswith(CLIP):
         model = CLIPForImageText(
