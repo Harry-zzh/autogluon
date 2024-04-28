52589ed1cf8f32e290b18b729710147fddfeb60a
Ubuntu <ubuntu@ip-172-31-9-160.us-west-2.compute.internal>
2024-04-28 04:21:22 +0000

    lemda loss weight and consist loss

--- multimodal/src/autogluon/multimodal/models/fusion/fusion_mlp.py
+++ multimodal/src/autogluon/multimodal/models/fusion/fusion_mlp.py
@@ -16,15 +16,18 @@ import torch.nn.functional as F
 logger = logging.getLogger(__name__)
 
 def consist_loss(p_logits, q_logits, threshold):
-    p = F.softmax(p_logits, dim=1)
-    logp = F.log_softmax(p_logits, dim=1)
-    logq = F.log_softmax(q_logits, dim=1)
-    loss = torch.sum(p * (logp - logq), dim=-1)
-    q = F.softmax(q_logits, dim=1)
-    q_largest = torch.max(q, dim=1)[0]
-    loss_mask = torch.gt(q_largest, threshold).float()
-    loss = loss * loss_mask
-    return torch.mean(loss)
+    if p_logits.size()[-1] == 1:
+        return F.mse_loss(p_logits, q_logits)
+    else:
+        p = F.softmax(p_logits, dim=1)
+        logp = F.log_softmax(p_logits, dim=1)
+        logq = F.log_softmax(q_logits, dim=1)
+        loss = torch.sum(p * (logp - logq), dim=-1)
+        q = F.softmax(q_logits, dim=1)
+        q_largest = torch.max(q, dim=1)[0]
+        loss_mask = torch.gt(q_largest, threshold).float()
+        loss = loss * loss_mask
+        return torch.mean(loss)
 
 def KL_loss(p_logits, q_logits): # 虽然写的是kl loss，但是对于regression任务也用不了。
     if p_logits.size()[-1] == 1: # regression
