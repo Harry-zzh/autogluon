5bbe04e9aaa8960c604f6e74feadaab2e02ebd58
Ubuntu <ubuntu@ip-172-31-3-160.us-west-2.compute.internal>
2024-06-10 15:39:21 +0000

    update numerical

--- multimodal/src/autogluon/multimodal/data/preprocess_dataframe.py
+++ multimodal/src/autogluon/multimodal/data/preprocess_dataframe.py
@@ -456,10 +456,7 @@ class MultiModalFeaturePreprocessor(TransformerMixin, BaseEstimator):
                 else: 
                     processed_data = col_value.apply(lambda ele: "" if pd.isnull(ele) else str(ele))
             elif col_type == NUMERICAL:
-                if self._config.numerical.convert_to_text_use_header:
-                    processed_data = pd.to_numeric(col_value).apply(lambda ele: "{}:{:.3f}".format(col_name, ele))
-                else:
-                    processed_data = pd.to_numeric(col_value).apply("{:.3f}".format)
+                processed_data = pd.to_numeric(col_value).apply("{:.3f}".format)
             elif col_type == f"{TEXT}_{IDENTIFIER}":
                 processed_data = col_value
             else:
