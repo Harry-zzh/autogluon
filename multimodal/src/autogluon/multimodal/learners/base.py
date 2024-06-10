6e5fd1fe8acafebbbf15d729626e6539eff81349
Ubuntu <ubuntu@ip-172-31-3-160.us-west-2.compute.internal>
2024-06-10 14:45:49 +0000

    gradient clip

--- multimodal/src/autogluon/multimodal/learners/base.py
+++ multimodal/src/autogluon/multimodal/learners/base.py
@@ -1096,6 +1096,9 @@ class BaseLearner(ExportMixin, DistillationMixin, RealtimeMixin):
         is_train=True,
     ):
         if is_train:
+            gradient_clip_val=OmegaConf.select(config, "optimization.gradient_clip_val", default=1)
+            if gradient_clip_val == "None":
+                gradient_clip_val = None
             blacklist_msgs = ["already configured with model summary"]
             log_filter = LogFilter(blacklist_msgs)
             with apply_log_filter(log_filter):
@@ -1112,7 +1115,7 @@ class BaseLearner(ExportMixin, DistillationMixin, RealtimeMixin):
                     max_time=max_time,
                     callbacks=callbacks,
                     logger=tb_logger,
-                    gradient_clip_val=OmegaConf.select(config, "optimization.gradient_clip_val", default=1),
+                    gradient_clip_val=gradient_clip_val,
                     gradient_clip_algorithm=OmegaConf.select(
                         config, "optimization.gradient_clip_algorithm", default="norm"
                     ),
